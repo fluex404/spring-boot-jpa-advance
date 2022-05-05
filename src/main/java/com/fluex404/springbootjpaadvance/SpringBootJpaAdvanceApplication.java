68560226986d3f84ccdb3d9d813cd79b4d10edbc
Isa Subani <isa.subani@nostratech.com>
2022-05-05 08:21:16 +0700

    first time

---
+++ src/main/java/com/fluex404/springbootjpaadvance/SpringBootJpaAdvanceApplication.java
@@ -0,0 +1,13 @@
+package com.fluex404.springbootjpaadvance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaAdvanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJpaAdvanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fluex404/springbootjpaadvance/SpringBootJpaAdvanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fluex404.springbootjpaadvance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJpaAdvanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
